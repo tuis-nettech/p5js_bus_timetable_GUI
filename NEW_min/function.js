8de4e9d3cda00c43f5ae4a924d068e3545ee827d
kbys-gnsk <urara.kbys@gmail.com>
2019-08-23 21:16:08 +0800

    Update function.js

--- NEW_min/function.js
+++ NEW_min/function.js
@@ -192,3 +192,37 @@ function drawLabelHigh(time,bus,position,PH) {
     }
 
 }
+
+/*
+function isHoliday(date) {
+  var req = new XMLHttpRequest();
+  req.open('get', 'syukujitsu.csv', true);
+  req.send(null);
+  req.onload = function() {
+    checkDate(req.responseText, date, function(result) {
+      console.log(result);
+    });
+  };
+}
+function checkDate(str, date, callback) {
+  var data = [];
+  var addZero = function(n) {
+    return ('0' + n).slice(-2);
+  }
+  var nDate = new Date(date);
+  var dataArr = [];
+  var checkDate = nDate.getFullYear() +
+            '-' + addZero(nDate.getMonth() + 1) +
+            '-' + addZero(nDate.getDate());
+  var isHoliday = false;
+  var tmp = str.split('\n');
+  for (var i=0; i< tmp.length; i++) {
+    dataArr = tmp[i].split(',');
+    if (dataArr[0] === checkDate) {
+      isHoliday = true;
+      break;
+    }
+  };
+  callback(isHoliday);
+}
+*/
